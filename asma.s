75310932efa6885295fbd7c65503efd11f6100ed
Maximilian Irro <max@disposia.org>
2014-03-16 12:08:03 +0100

    actually, it is andnps, not andps

--- asma.s
+++ asma.s
@@ -15,16 +15,16 @@ asma:
 	# we can live with that, can't we
 	movdqu (%rdi),%xmm8  		# load s into xmm8
 	movdqu (%rsi),%xmm9		# load t into xmm9
-	andps mask, %xmm8  		# xmm8 = ~xmm8 & mask, invert bits 
-	andps mask, %xmm9  		# xmm9 = ~xmm9 & mask, invert bits
+	andnps mask, %xmm8  		# xmm8 = ~xmm8 & mask, invert bits 
+	andnps mask, %xmm9  		# xmm9 = ~xmm9 & mask, invert bits
 	movdqu %xmm8,%xmm10		# make a copy of (NOT s)
 	pminub %xmm8, %xmm9		# compare 16x8 bit blocks (the chars), write 
 					# 0xFF to xmm9 if block in xmm9 is smaller
-					# than the one in xmm10, else write 0x00
-	andps %xmm8, %xmm10			# mask values in (NOT s)
-	andps %xmm8, %xmm9			# mask values in (NOT t)
+					# than the one in xmm8, else write 0x00
+	andnps %xmm8, %xmm10		# mask values in (NOT s)
+	andnps %xmm8, %xmm9		# mask values in (NOT t)
 
-	orpd %xmm10, %xmm9			# u = s | t
+	orpd %xmm10, %xmm9		# u = s | t
 	movdqu %xmm9, (%rax)
 	ret
 	
