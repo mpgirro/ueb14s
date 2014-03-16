26f4db54f77811db3dacf4610aaf1f92826b4010
Maximilian Irro <max@disposia.org>
2014-03-16 11:46:53 +0100

    changed the mov and or commands back to 128 operations

--- asma.s
+++ asma.s
@@ -13,18 +13,18 @@ asma:
 	# fortunatelly, this is eaqual to:
 	# NOT u[i] = (NOT s[i] < NOT t[i]) ? NOT s[i] : NOT t[i];
 	# we can live with that, can't we
-	movq (%rdi),%xmm8  		# load s into xmm8
-	movq (%rsi),%xmm9		# load t into xmm9
+	movdqu (%rdi),%xmm8  		# load s into xmm8
+	movdqu (%rsi),%xmm9		# load t into xmm9
 	andps mask, %xmm8  		# xmm8 = ~xmm8 & mask, invert bits 
 	andps mask, %xmm9  		# xmm9 = ~xmm9 & mask, invert bits
-	movq %xmm8,%xmm10		# make a copy of (NOT s)
+	movdqu %xmm8,%xmm10		# make a copy of (NOT s)
 	pminub %xmm8, %xmm9		# compare 16x8 bit blocks (the chars), write 
 					# 0xFF to xmm9 if block in xmm9 is smaller
 					# than the one in xmm10, else write 0x00
 	andps %xmm8, %xmm10			# mask values in (NOT s)
 	andps %xmm8, %xmm9			# mask values in (NOT t)
 
-	por %xmm10, %xmm9			# u = s | t
-	movq %xmm9, %rax
+	orpd %xmm10, %xmm9			# u = s | t
+	movdqu %xmm9, (%rax)
 	ret
 	
