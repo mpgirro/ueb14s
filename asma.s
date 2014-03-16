cfa733bb8fe3a7b4f60cbac44fabfaa05763501d
Maximilian Irro <max@disposia.org>
2014-03-16 10:59:18 +0100

    changed mov commands, still segfault

--- asma.s
+++ asma.s
@@ -13,17 +13,18 @@ asma:
 	# fortunatelly, this is eaqual to:
 	# NOT u[i] = (NOT s[i] < NOT t[i]) ? NOT s[i] : NOT t[i];
 	# we can live with that, can't we
-	movdqu (%rdi),%xmm8  		# load s into xmm8
-	movdqu (%rsi),%xmm9			# load t into xmm9
+	movq (%rdi),%xmm8  		# load s into xmm8
+	movq (%rsi),%xmm9			# load t into xmm9
 	andps mask, %xmm8  			# xmm8 = ~xmm8 & mask, invert bits 
 	andps mask, %xmm9  			# xmm9 = ~xmm9 & mask, invert bits
-	movdqu %xmm8,%xmm10		# make a copy of (NOT s)
+	movq %xmm8,%xmm10		# make a copy of (NOT s)
 	pminub %xmm8, %xmm9			# compare 16x8 bit blocks (the chars), 
 								# write 0xFF to xmm9 if the block in xmm9 is
 								# smaller than the one in xmm10, else write 0x00
-	andpd %xmm8, %xmm10			# mask values in (NOT s)
-	andpd %xmm8, %xmm9			# mask values in (NOT t)
-	orpd %xmm9, %xmm10			# u = s | t
-	movdqu (%xmm10),%rax  		# load u to return register		
+	andps %xmm8, %xmm10			# mask values in (NOT s)
+	andps %xmm8, %xmm9			# mask values in (NOT t)
+
+	por %xmm10, %xmm9			# u = s | t
+	movq %xmm9, %rax
 	ret
 	
