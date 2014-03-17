f777fc163b1750c50d92da1e35428e3ad8f02b4c
Maximilian Irro <max@disposia.org>
2014-03-17 12:30:22 +0100

    updated comments

--- asma.s
+++ asma.s
@@ -1,6 +1,7 @@
 	.globl asma
 	.type asma, @function
-	
+
+# make 128 bits worth of 1s - we need that as a mask
 mask: 
 	.align
 	.size mask, 16
@@ -13,15 +14,15 @@ asma:
 	# fortunatelly, this is eaqual to:
 	# NOT u[i] = (NOT s[i] < NOT t[i]) ? NOT s[i] : NOT t[i];
 	# we can live with that, can't we
-	movdqu (%rdi),%xmm8  		# load s into xmm8
+	movdqu (%rdi),%xmm8		# load s into xmm8
 	movdqu (%rsi),%xmm9		# load t into xmm9
-	#movdqu $0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF, %xmm11  		# xmm8 = ~xmm8 & mask, invert bits 
-	movdqu mask, %xmm12		# load mask to xmm12
-	andnpd %xmm12, %xmm8 		# xmm8 = -xmm8 & mask => s = -s	
-	andnpd %xmm12, %xmm9		# xmm9 = -xmm9 & mask => t = -t
+	movdqu mask, %xmm10		# load mask to xmm10
+	andnpd %xmm10, %xmm8	# xmm8 = -xmm8 & mask => s = -s	
+	andnpd %xmm10, %xmm9	# xmm9 = -xmm9 & mask => t = -t
 	pminub %xmm8, %xmm9		# compare 8 bit blocks, write the lesser 
-					# block into %xmm9 
-	andnpd %xmm12, %xmm9
-	movdqu %xmm9, (%rdx)	
+							# block into %xmm9. for we inverted the blocks
+							# before, now it always holds the bigger block
+	andnpd %xmm10, %xmm9	# we have to re-invert 
+	movdqu %xmm9, (%rdx)	# write it to result field
 	ret
 	
