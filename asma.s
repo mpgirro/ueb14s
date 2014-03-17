940567e973eecfe998968ceaea859cd0b0bdfd9f
Maximilian Irro <max@disposia.org>
2014-03-17 08:57:31 +0100

    inverting seems to work now, one has to use a xmm register to do so

--- asma.s
+++ asma.s
@@ -1,10 +1,10 @@
 	.globl asma
 	.type asma, @function
 	
-#mask: 
-#	.align
-#	.size mask, 16
-#	.fill 16, 1, 0xFF	
+mask: 
+	.align
+	.size mask, 16
+	.fill 16, 1, 0xFF	
 	
 asma:
 	# well, we have to compare characters like this:
@@ -14,17 +14,23 @@ asma:
 	# NOT u[i] = (NOT s[i] < NOT t[i]) ? NOT s[i] : NOT t[i];
 	# we can live with that, can't we
 	movdqu (%rdi),%xmm8  		# load s into xmm8
+
+	
+
 	movdqu (%rsi),%xmm9		# load t into xmm9
-	andnpd 0xFFFFFFFFFFFFFFFF, %xmm8  		# xmm8 = ~xmm8 & mask, invert bits 
-	andnpd 0xFFFFFFFFFFFFFFFF, %xmm9  		# xmm9 = ~xmm9 & mask, invert bits
-	movdqu %xmm8,%xmm10		# make a copy of (NOT s)
-	pminub %xmm8, %xmm9		# compare 16x8 bit blocks (the chars), write 
+	#movdqu $0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF, %xmm11  		# xmm8 = ~xmm8 & mask, invert bits 
+	movdqu mask, %xmm11
+	andnpd %xmm11, %xmm8	
+	movdqu %xmm8, (%rdx)	
+#andnpd 0xFFFFFFFFFFFFFFFF, %xmm9  		# xmm9 = ~xmm9 & mask, invert bits
+	#movdqu %xmm8,%xmm10		# make a copy of (NOT s)
+	#pminub %xmm8, %xmm9		# compare 16x8 bit blocks (the chars), write 
 					# 0xFF to xmm9 if block in xmm9 is smaller
 					# than the one in xmm8, else write 0x00
-	andnpd %xmm8, %xmm10		# mask values in (NOT s)
-	andnpd %xmm8, %xmm9		# mask values in (NOT t)
+	#andnpd %xmm8, %xmm10		# mask values in (NOT s)
+	#andnpd %xmm8, %xmm9		# mask values in (NOT t)
 
-	orpd %xmm10, %xmm9		# u = s | t
-	movdqu %xmm9, (%rax)
+	#orpd %xmm10, %xmm9		# u = s | t
+	#movdqu %xmm9, (%rax)
 	ret
 	
