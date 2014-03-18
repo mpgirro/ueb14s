527ec622d0bfd4c8cf88640d34a76a86e60dae17
Maximilian Irro <max@disposia.org>
2014-03-18 21:24:45 +0100

    changes main.c to the official test script. don't know how if this will be looked at at the end. better safe than sorry

--- main.c
+++ main.c
@@ -1,56 +1,113 @@
-/**
- * @file main.c
- * @author Maximilian Irro <e1026859@student.tuwien.ac.at>
- * @date last revision 2014-03-17
- * @brief testpogram for Uebersetzerbau Bsp1 "Assambler A"
- */
-
-/* === librarys === */
-
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <assert.h>
-#include <stdarg.h>
+#include <mcheck.h>
+#include <stdlib.h>
 
-void print_hex(const char *varname, unsigned char *s);
-
-/* === externals === */
 extern void asma(unsigned char *s, unsigned char *t, unsigned char *u);
+  
+//extern uac_sigbus();
+extern void asma_callchecking(unsigned char *s, unsigned char *t, unsigned char *u);
 
+void orig_asma(unsigned char *s, unsigned char *t, unsigned char *u)  
+{  
+  int i;  
+  for (i=0; i<16; i++)  
+    u[i] = (s[i]>t[i]) ? s[i] : t[i];  
+} 
+
+void printarray(unsigned char* bufstart, int buflength, unsigned char* s)
+{
+  int i;
+  unsigned char *p=s-16;
+  unsigned long pl;
+  unsigned long l=16;
+  if (p<bufstart)
+    p=bufstart;
+  pl=s+l+16-p;
+  if (p+pl>bufstart+buflength)
+    pl=bufstart+buflength-p;
+  printf("%p=",p);
+  for(i=0; ;i++) {
+    if (p+i==s+l)
+      printf("]");
+    if (!(i<pl))
+      break;
+    if(p+i==s)
+      printf("[");
+    printf("%02x",p[i]);
+  }
+  printf("\n");
+}
+
+int test(unsigned char* s,unsigned char* t, unsigned char* u,
+	 unsigned long sa,unsigned long ta, unsigned long ua)
+{
+  unsigned long l=16;
+  unsigned char stmp[l+16];
+  unsigned char ttmp[l+16];
+  unsigned char utmp[l+16];
+
+  // init
+  int i;
+  int off=sa+ta+ua*8;
+  for (i=0; i<l+16; i++) {
+    s[i]=i*14*off;
+    t[i]=256-i*15+off;
+  }
+
+  // check
+  memcpy(stmp,s,l+16);
+  memcpy(ttmp,t,l+16);
+  memset(u,0xff,l);
+  memset(utmp,0xff,l);
+  printf("\nCalling asma(%p,%p,%p) with\n",s+sa,t+ta,u+ua);
+  printarray(s+sa,l,s+sa);
+  printarray(t+ta,l,t+ta);
+  printf("Result:\n");
+  asma_callchecking(s+sa,t+ta,u+ua);
+
+  printarray(u+ua,16,u+ua);
+
+  orig_asma(stmp+sa,ttmp+ta,utmp+ua);
+
+  if (memcmp(utmp+ua,u+ua,16)!=0) {
+    printf("failed: return value wrong. Expected:\n");
+    printarray(utmp+ua,l,utmp+ua);
+    return 0;
+  } else if (memcmp(stmp,s,l+16)!=0) {
+    printf("failed: s modified\n");
+    printarray(s,l+16,s+sa);
+    printarray(stmp,l+16,stmp+sa);
+    return 0;
+  } else if (memcmp(ttmp,t,l+16)!=0) {
+    printf("failed: t modified\n");
+    printarray(ttmp,l+16,ttmp+ta);
+    printarray(ttmp,l+16,ttmp+ta);
+    return 0;
+  } else {
+    printf("succeeded\n");
+    return 1;
+  }
+}
 
 int main(int argc, char **argv)
 {
-	
-	unsigned char 	*s, *t, *u;
-	s = malloc(17);
-	t = malloc(17);
-	u = malloc(17);
-					
-	strncpy((char *) s, "abcdehfghijklmno", 16);
-	strncpy((char *) t, "yjhdflkffkdkfkfv", 16);
-	
-	print_hex("s",s);
-	print_hex("t",t);
+  int success=1;
+  int l=16+16;
+  int i,j,k;
 
-	asma(s,t,u);
-	u[16] = '\0';
-	
-	print_hex("u",u);
-	
-	free(s);
-	free(t);
-	free(u);
-	
-	return 0;
-}
+  unsigned char s[l];
+  unsigned char t[l];
+  unsigned char u[l];
 
-void print_hex(const char *varname,unsigned char *s)
-{
-	(void) printf("%s: %s (0x",varname,s);
-	while(*s)
-	{
-		(void) printf("%02x",(unsigned int) *s++);
-	}
-	(void) printf(")\n");
+  for(i=0; i<3;i++)
+    for(j=0; j<3;j++)
+      for(k=0; k<3;k++)
+	success&=test(s,t,u,i,j,k);
+
+  if (!success)
+    fprintf(stdout,"\nTest failed.\n");
+  else
+    fprintf(stdout,"\nTest succeeded.\n");
+  return !success;
 }
