22a67efe4eb470552508b7f2f9e91c666c08f691
Maximilian Irro <max@disposia.org>
2014-07-01 09:41:52 +0200

    pretty comments

--- asma.s
+++ asma.s
@@ -14,14 +14,14 @@ asma:
 	# fortunatelly, this is eaqual to:
 	# NOT u[i] = (NOT s[i] < NOT t[i]) ? NOT s[i] : NOT t[i];
 	# we can live with that, can't we
-	movdqu (%rdi),%xmm8		# load s into xmm8
-	movdqu (%rsi),%xmm9		# load t into xmm9
-	movdqu mask, %xmm10		# load mask to xmm10
+	movdqu (%rdi),%xmm8	# load s into xmm8
+	movdqu (%rsi),%xmm9	# load t into xmm9
+	movdqu mask, %xmm10	# load mask to xmm10
 	andnpd %xmm10, %xmm8	# xmm8 = -xmm8 & mask => s = -s	
 	andnpd %xmm10, %xmm9	# xmm9 = -xmm9 & mask => t = -t
-	pminub %xmm8, %xmm9		# compare 8 bit blocks, write the lesser 
-							# block into %xmm9. for we inverted the blocks
-							# before, now it always holds the bigger block
+	pminub %xmm8, %xmm9	# compare 8 bit blocks, write the lesser 
+				# block into %xmm9. for we inverted the blocks
+				# before, now it always holds the bigger block
 	andnpd %xmm10, %xmm9	# we have to re-invert 
 	movdqu %xmm9, (%rdx)	# write it to result field
 	ret
